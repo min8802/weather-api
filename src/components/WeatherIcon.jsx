101a1f14b8f2da950daf7f733e3e78402fff4dcd
min8802 <ww330@naver.com>
2024-05-13 16:35:31 +0900

    Geolocation, Axios

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import axios from "axios";
 
 const WeatherIcon = () => {
     const [latitude, setLatitude] = useState();
     const [longitude, setLongitude] = useState();
+
+    const getWeather = async () => {
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${import.meta.env.VITE_API_KEY}&units=metric`);
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -12,6 +22,12 @@ const WeatherIcon = () => {
          setLongitude(position.coords.longitude); 
         });
     }, []);
+
+    useEffect(() => {
+        if (!latitude) return;
+    
+        getWeather();
+      }, [latitude]);
     
     return <div>WeatherIcon</div>;
   };
