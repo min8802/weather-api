9ebf8e32f0cd1a81a400e7a096220498dc576966
min8802 <ww330@naver.com>
2024-05-13 15:31:16 +0900

    langitude, longitude

---
+++ src/components/WeatherIcon.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+const WeatherIcon = () => {
+    const [latitude, setLatitude] = useState();
+    const [longitude, setLongitude] = useState();
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+         console.log(position.coords.latitude);
+         setLatitude(position.coords.latitude);
+         setLongitude(position.coords.longitude); 
+        });
+    }, []);
+    
+    return <div>WeatherIcon</div>;
+  };
+  
+  export default WeatherIcon;
